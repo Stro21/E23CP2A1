6fc81a9dc39f73039e9fd8f6c69bf2a00e24de22
Eduardo Straub <eduardo.straub1@gmail.com>
2019-04-24 21:15:14 -0400

    Se avanza en el ejercicio

---
+++ desafio46.js
@@ -0,0 +1,10 @@
+function btn() {
+  var selector = document.getElementById("camposelector").value;
+  if (selector === "") {
+    alert("Debe ingresar un selector jQuery!")
+  }
+  else{
+    
+  }
+  console.log(selector)
+}
